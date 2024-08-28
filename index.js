f8b3e738a478d18a452a56ee24f346203ef10890
Loreto <loreto.garde@gmail.com>
2024-08-28 16:03:11 +0200

    Solved lab

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} `;
 
 // Print out the concatenated string
-
+console.log(tongueTwister);
 
 
 
@@ -22,10 +22,10 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+let result = part1.slice(0, - 1) + part1[part1.length - 1].toUpperCase() + part2.slice(0, - 1) + part2[part2.length - 1].toUpperCase();
 
 // Print the cameLtaiL-formatted string
-
+console.log(result);
 
 
 
@@ -35,10 +35,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = billTotal * 15 / 100;
 
 // Print out the tipAmount
-
+console.log(tipAmount);
 
 
 
@@ -47,10 +47,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const numRandom = Math.floor(Math.random() * 11);
 
 // Print the generated random number
-
+console.log(numRandom);
 
 
 /*******************************************
